a0094130d6c1d23d34cf2dd2ef025cf0bf9f8b47
Alexoqool <154623493+Alexoqool@users.noreply.github.com>
2025-03-03 15:59:47 +0300

    MainActivity: Format code

--- app/src/main/java/com/jetflex/MainActivity.kt
+++ app/src/main/java/com/jetflex/MainActivity.kt
@@ -7,37 +7,40 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.jetflex.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
-    
+public class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
+
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding
         get() = checkNotNull(_binding) { "Activity has been destroyed" }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         window.decorView.apply {
-        systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-    }
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
         binding.surfaceView.holder.addCallback(this)
     }
-    
+
     override fun surfaceCreated(holder: SurfaceHolder) {
         Log.d("MainActivity", "Surface Created")
         // TODO
     }
-    
+
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
         Log.d("MainActivity", "Surface Changed: width=$width, height=$height")
         // TODO
     }
-    
+
     override fun surfaceDestroyed(holder: SurfaceHolder) {
         Log.d("MainActivity", "Surface Destroyed")
         // TODO
     }
-    
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
