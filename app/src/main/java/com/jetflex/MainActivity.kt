7d48a3277b6c66a3731ddb25aae76827bbd2d7e5
Alexoqool <154623493+Alexoqool@users.noreply.github.com>
2025-03-03 15:25:16 +0300

    MainActivity: Init SurfaceView

--- app/src/main/java/com/jetflex/MainActivity.kt
+++ app/src/main/java/com/jetflex/MainActivity.kt
@@ -1,27 +1,41 @@
 package com.jetflex
 
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.jetflex.databinding.ActivityMainBinding
 
-public class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     
     private var _binding: ActivityMainBinding? = null
-    
     private val binding: ActivityMainBinding
-      get() = checkNotNull(_binding) { "Activity has been destroyed" }
+        get() = checkNotNull(_binding) { "Activity has been destroyed" }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        setSupportActionBar(binding.toolbar)
-        
-        binding.fab.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Replace with your action", Toast.LENGTH_SHORT).show()
-        }
+        window.decorView.apply {
+        systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    }
+        binding.surfaceView.holder.addCallback(this)
+    }
+    
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Log.d("MainActivity", "Surface Created")
+        // TODO
+    }
+    
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        Log.d("MainActivity", "Surface Changed: width=$width, height=$height")
+        // TODO
+    }
+    
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        Log.d("MainActivity", "Surface Destroyed")
+        // TODO
     }
     
     override fun onDestroy() {
